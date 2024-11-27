0d871732c977cffd86ed719585e4bb9fddb9fd17
0-SHINN <hys3448@naver.com>
2024-11-28 00:41:00 +0900

    퍼널별 전환율

---
+++ Fashion-Campus/Code/funnel.sql
@@ -0,0 +1,6 @@
+-- Funnel
+SELECT COUNT(CASE WHEN event_name = 'HOMEPAGE' THEN session_id END) AS homepage_count,
+       COUNT(CASE WHEN event_name = 'ITEM_DETAIL' THEN session_id END) AS pageview_count,
+       COUNT(CASE WHEN event_name = 'ADD_TO_CART' THEN session_id END) AS addtocart_count,
+       COUNT(CASE WHEN event_name = 'BOOKING' THEN session_id END) AS purchase_count
+  FROM `click_stream`
