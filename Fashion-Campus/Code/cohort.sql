4025a4a4f07c4bde6bc064a441555723037396e7
0-SHINN <hys3448@naver.com>
2024-11-28 01:13:07 +0900

    재구매율 리텐션

--- Fashion-Campus/Code/cohort.sql
+++ Fashion-Campus/Code/cohort.sql
@@ -3,8 +3,7 @@ WITH first_join AS(
   SELECT c.first_join_date,
          c.customer_id
   FROM `customers` AS c
-  LEFT JOIN `transaction` AS t 
-			   ON c.customer_id = t.customer_id
+  LEFT JOIN `transaction` AS t ON c.customer_id = t.customer_id
   GROUP BY c.customer_id, first_join_date
   ORDER BY c.customer_id
  ),
@@ -14,12 +13,13 @@ WITH first_join AS(
          DATE_DIFF(DATE(created_at), first_join_date, MONTH) AS month_diff,
          DATE_TRUNC(first_join_date, MONTH) AS first_join_month
   FROM `transaction` AS t
-  LEFT JOIN first_join AS f 
-			   ON t.customer_id = f.customer_id
+  LEFT JOIN first_join AS f ON t.customer_id = f.customer_id
  )
 SELECT first_join_month,
        month_diff,
-       COUNT(DISTINCT customer_id) / FIRST_VALUE(COUNT(DISTINCT(customer_id) OVER (PARTITION BY first_join_month ORDER BY month_diff ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) * 100 AS retention
+       COUNT(DISTINCT customer_id) / FIRST_VALUE(COUNT(DISTINCT(customer_id))) 
+       OVER (PARTITION BY first_join_month ORDER BY month_diff 
+       ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) * 100 AS retention
 FROM cohort
 GROUP BY first_join_month, month_diff
 ORDER BY first_join_month, month_diff
