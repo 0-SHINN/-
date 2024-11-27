1df2dca7d92d2dc4e1f0d4126cb64f779d84e055
0-SHINN <hys3448@naver.com>
2024-11-28 01:05:10 +0900

    월별 ARPPU

--- Fashion-Campus/Code/ARPPU.sql
+++ Fashion-Campus/Code/ARPPU.sql
@@ -1,8 +1,6 @@
 -- 월별 ARPPU
 SELECT DATE_TRUNC(DATE(created_at), MONTH) AS month,
-			 CAST(SUM(total_amount) 
-			 / COUNT(DISTINCT customer_id) AS INTEGER) 
-			 AS ARPPU
+       CAST(SUM(total_amount) / COUNT(DISTINCT customer_id) AS INTEGER) AS ARPPU,
        COUNT(DISTINCT customer_id) AS Paying_user,
        SUM(item_price * quantity) AS revenue
 FROM `transaction`
